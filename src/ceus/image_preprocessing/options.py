987f4cf1bed8e12e0aed2ae1800d0b7d5f2ce1a1
omid <ochaghaneh@gmail.com>
2026-01-22 11:44:01 +0100

    Fix module path in options.py and uncomment functions in functions.py

--- src/ceus/image_preprocessing/options.py
+++ src/ceus/image_preprocessing/options.py
@@ -10,7 +10,7 @@ def get_im_preproc_funcs() -> dict:
     Returns:
         dict: Dictionary of preprocessing functions.
     """
-    functions = {name: obj for name, obj in globals().items() if callable(obj) and obj.__module__ == 'src.image_preprocessing.functions'}
+    functions = {name: obj for name, obj in globals().items() if callable(obj) and obj.__module__ == 'src.ceus.image_preprocessing.functions'}
     return functions
 
 def get_required_im_preproc_kwargs(preproc_func_names: list) -> list:
